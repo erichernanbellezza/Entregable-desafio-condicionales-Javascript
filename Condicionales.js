c2b6286f5823779c7c7fddf2b8c4de12b60bd200
Eric <erichernan27@hotmail.com>
2021-09-03 20:35:29 -0300

    Archivo.js

---
+++ Condicionales.js
@@ -0,0 +1,14 @@
+let edad = parseInt(prompt("Cual es tu edad?"));
+
+if (edad >= 72 && edad <= 82 ) {
+    alert("Hola querido señor de la Silent Generation!");
+} else if(edad >= 53 && edad <= 72 ) {
+    alert("Perteneces a la generación Baby Boom!");
+} else if (edad >= 41 && edad <= 53) { 
+    alert("Saludos miembro de la generación X !");
+} else if (edad >= 27 && edad <= 41) {
+    alert("Bienvenido miembro Millenial!");
+} else if (edad >= 11 && edad <= 27) {
+    alert("Sos de la generación Z!");
+}
+  
