39268d8a7f10229645e40953040230faa574a409
Eric Hernán Bellezza <erichernan27@hotmail.com>
2021-09-10 15:32:52 -0300

    Update simuladorInteractivo.js

--- simuladorInteractivo.js
+++ simuladorInteractivo.js
@@ -1,29 +1,3 @@
-
-/*alert ("Aplicación para calcular IVA");
-
-
-let numeroIngresado = parseInt(prompt("Ingresa el monto para calcular el IVA del 21%"));
-
-if (numeroIngresado === Number) {
-    function calcularIva21() {
-        let valorConIva = numeroIngresado * 21/100 ;
-        console.log(valorConIva);
-        alert("El iva es" + " " + valorConIva);
-    };
-    calcularIva21();
-}
-
-
-function calcularIva10() {
-    let numeroIngresado = parseInt(prompt("Ingresa el monto para calcular el IVA del 10%"));
-    let valorConIva = numeroIngresado * 10/100 ;
-    console.log(valorConIva);
-    alert("El iva es" + " " + valorConIva);
-
-    calcularIva10();
-}; */
-
-
 while (true) {
     let numeroIngresado = parseInt(prompt("Ingresa un número"));
     
